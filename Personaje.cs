b592dbdd06c4556aa474752607adc0a11d07a59e
sergio - cuervo - 98 <94074238+sergio-cuervo-l@users.noreply.github.com>
2024-08-15 17:43:11 -0500

    Crear proyecto de simulación de batallas
    Se sube el programa de consola donde se puede observar la batalla entre Deadpool y Wolverine, donde se atacan de manera aleatoria

---
+++ Personaje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeleaPersonajes
+{
+    public class Personaje
+    {
+        public Personaje(string Name, int PS, int Dmg, double Esq)
+        {
+            this.Name = Name;
+            this.PuntosSalud = PS;
+            this.DamageMaximo = Dmg;
+            this.Esquivar = Esq;
+        }
+
+        public string Name { get; set; }
+        public int PuntosSalud { get; set; }
+        public int DamageMaximo { get; set; }
+        public double Esquivar { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace PeleaPersonajes
+{
+    public class Program
+    {
+
+        public bool atarcarPeronsaje(bool regenerate, Personaje ataque, Personaje defensa)
+        {
+            Random rnd = new Random();
+            if (regenerate)
+            {
+                Console.WriteLine($"{ataque.Name} se está regenerando y no ataca.");
+                regenerate = false;
+            }
+            else if (rnd.NextDouble() > defensa.Esquivar)
+            {
+                int damage = rnd.Next(10, ataque.DamageMaximo);
+                Console.WriteLine($"{ataque.Name} ataca a {defensa.Name} con {damage} de daño.");
+                if (damage == 100)
+                {
+                    regenerate = true;
+                    Console.WriteLine($"¡Golpe Critico de {ataque.Name}! {defensa.Name} no atacará en el siguiente ya que tiene que regenerarse.");
+                }
+                defensa.PuntosSalud -= damage;
+                if (defensa.PuntosSalud <= 0)
+                {
+                    Console.WriteLine($"La vida de {defensa.Name} ha llegado a cero.");
+                }
+                else
+                {
+                    Console.WriteLine($"Vida restante de {defensa.Name}: {defensa.PuntosSalud}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"¡{defensa.Name} esquiva el ataque de {ataque.Name}!");
+            }
+            return regenerate;
+        }
+
+        public void simularBatalla(int turn, Personaje deadpool, Personaje wolverine)
+        {
+            bool regenerate = false;
+
+            if (deadpool.PuntosSalud > 0 && wolverine.PuntosSalud > 0)
+            {
+                turn++;
+                Console.WriteLine($"Turno Nro. {turn}");
+                atarcarPeronsaje(regenerate, deadpool, wolverine);
+                atarcarPeronsaje(regenerate, wolverine, deadpool);
+                simularBatalla(turn, deadpool, wolverine);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Random rnd = new Random();
+            Console.WriteLine("Introduce la vida de Deadpool");
+            int deadpool_health = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce la vida de Wolverine");
+            int wolverine_health = int.Parse(Console.ReadLine());
+
+            Personaje deadpool = new Personaje("Deadpool", deadpool_health, 100, 0.25);
+            Personaje wolverine = new Personaje("Wolverine", deadpool_health, 120, 0.2); 
+
+            p.simularBatalla(0, deadpool, wolverine);
+
+        }
+    }
+}
